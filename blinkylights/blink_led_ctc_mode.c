4f05704c232277c50ddca39bbd940a0dd8587384
amar98-hash <dadel.amar98@gmail.com>
2022-10-12 04:14:30 -0800

    changed and added directories and files

---
+++ blinkylights/blink_led_ctc_mode.c
@@ -0,0 +1,59 @@
+/*
+ * _8bittimer.c
+ *
+ * Created: 10/3/2021 7:53:07 PM
+ *  Author: Amar Dadel
+ */ 
+
+#include <avr/io.h>
+
+#define F_CPU 16000000UL    //Define CPU clock as 16Mhz
+
+#include <util/delay.h>
+
+#include <avr/interrupt.h>
+
+int extraTime=0;
+
+int main(void)
+
+{
+
+	DDRB=0x01;
+	TCCR0A=(1<<WGM01);//sets ctc bits, CTC = clear timer on compare.
+	OCR0A=156;//compares the counter with 156 ticks.
+	
+	TIMSK0=(1<<OCIE0A);
+	sei(); // sets I bit for interrupt to occur.
+	
+	TCCR0B=(1<<CS02)|(1<<CS00);// start at prescalar of 1024.
+	
+	
+	
+
+	//DDRB= (1<<DDB1)|(1<<DDB2)|(1<<DDB3)|(0x00);
+while(1){
+	
+	
+	
+}
+	//add your codes here
+
+}
+
+//The interrupt service routine are written as follow:
+
+ISR(TIMER0_COMPA_vect)
+
+{
+	
+	extraTime++;
+	if(extraTime>1000){
+		PORTB^=(1<<PORTB0)
+		
+	}	
+	
+	
+	
+	
+}
